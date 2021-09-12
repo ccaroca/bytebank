07f3dd5de4a62b20945e4b60f8904138622ee5eb
CAIO REGIS CAROCA <ccaroca@trt13.jus.br>
2021-09-12 11:26:46 -0300

    Utilizar o event binding p capturar o ev propag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any = {};
+  mensagem: string = '';
+
+  transferir($event: any) {
+    console.log($event);
+    this.transferencia = $event;
+  }
+
+  exibirModalErro($event: any) {
+    // implementação da lógica para exibir o modal
+    console.log($event);
+    this.mensagem = $event;
+  }
 }
